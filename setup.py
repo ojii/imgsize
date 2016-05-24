50c182fe1c15ca3d37aec2efc4fd6eae2669a75a
Jonas Obrist <jonas.obrist@hde.co.jp>
2016-05-24 19:19:15 +0900

    version 1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='imgsize',
     author='Jonas Obrist',
     license='BSD',
-    version='1.0',
+    version='1.1',
     test_suite='imgsize.tests',
     packages=find_packages(),
     classifiers=[
