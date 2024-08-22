e995c3d6bcdf243a9f842817a9d8c4fd6864dbf7
Jonas Obrist <jonas.obrist@hennge.com>
2024-08-22 15:52:22 +0900

    Fix GIF parser
    Previously, the GIF parser would assume a GIF is animated if it had the
    Netscape Application Extension. This can lead to false positives where a
    GIF can have all the indicators of an animated GIF but only one Image
    Descriptor.
    
    Now, the code checks for the absence of the Graphic Control Extension
    before an Image Descriptor to mean non-animated and otherwise checks if
    there are multiple Image Descriptors. This unfortunately requires
    parsing much further into the data stream.
    
    The example cli now also supports specifiying the buffer size.

--- python-tests/test_sample_files.py
+++ python-tests/test_sample_files.py
@@ -1,9 +1,7 @@
 import json
-
 import pytest
-from conftest import ROOT
 
-BYTES_TO_READ = 1024
+from conftest import ROOT
 
 
 def find_examples():
@@ -12,7 +10,7 @@ def find_examples():
         output_path = input_path.with_suffix('.output')
         if not output_path.exists(): continue
         with input_path.open('rb') as fobj:
-            data = fobj.read(BYTES_TO_READ)
+            data = fobj.read()
         with output_path.open('r') as fobj:
             output = json.load(fobj)
         yield pytest.param(data, output, id=input_path.stem)
